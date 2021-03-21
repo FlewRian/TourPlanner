5078e326af7d2f83a5cd426896ddccd839dbd270
FlewXD <weiskirchner-florian@gmx.at>
2021-03-21 19:41:03 +0100

    TourAddView wurde erstellt und angefügt und kleine Änderungen an der UI

---
+++ TourPlanner/TourPlanner/TourAddWindowFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace TourPlanner
+{
+    class TourAddWindowFactory : IWindowFactory
+    {
+        public static void CreateWindow()
+        {
+            TourAddWindow view = new TourAddWindow();
+            view.Show();
+        }
+
+        void IWindowFactory.CreateWindow()
+        {
+            CreateWindow();
+        }
+    }
+
+}
